fff2082b73f86174817cec3b34fea93e025db1df
Chuan Shao <shaochuancs@gmail.com>
2021-02-19 16:23:48 +0800

    enable test coverage in jest

--- jest.config.js
+++ jest.config.js
@@ -17,10 +17,10 @@ module.exports = {
   // clearMocks: false,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: ['src/**/*.{ts,js}'],
 
   // The directory where Jest should output its coverage files
   // coverageDirectory: "coverage",
